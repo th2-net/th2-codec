d436c9a8470e44d1f73db0cd0411bcf23501a801
Nikita Shaposhnikov <topru123@gmail.com>
2022-02-02 12:17:17 +0300

    copyright update

--- src/main/kotlin/com/exactpro/th2/codec/Main.kt
+++ src/main/kotlin/com/exactpro/th2/codec/Main.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
+ * Copyright 2020-2022 Exactpro (Exactpro Systems Limited)
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -70,7 +70,7 @@ class CodecCommand : CliktCommand() {
 
             val rootEventId = eventRouter.storeEvent(
                 Event.start().apply {
-                    name("Codec_${applicationContext.protocols}_${LocalDateTime.now()}")
+                    name("Codec protocols: ${applicationContext.protocols.joinToString(",")} ${LocalDateTime.now()}")
                     type("CodecRoot")
                 }
             ).id
