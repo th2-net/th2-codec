40cd8515446808ca0c22931d19b39a880959a057
oleg.smirnov <oleg.smirnov@exactprosystems.com>
2022-02-21 15:54:04 +0300

    Use reference to event instead of separate event to attached parent event ID

--- src/main/kotlin/com/exactpro/th2/codec/AbstractCodecProcessor.kt
+++ src/main/kotlin/com/exactpro/th2/codec/AbstractCodecProcessor.kt
@@ -23,6 +23,7 @@ import com.exactpro.th2.common.event.Event.Status
 import com.exactpro.th2.common.event.Event.Status.FAILED
 import com.exactpro.th2.common.event.Event.Status.PASSED
 import com.exactpro.th2.common.event.EventUtils
+import com.exactpro.th2.common.event.IBodyData
 import com.exactpro.th2.common.grpc.MessageGroupBatch
 import com.exactpro.th2.common.grpc.MessageID
 import mu.KotlinLogging
@@ -41,33 +42,41 @@ abstract class AbstractCodecProcessor(
         message: String,
         messagesIds: List<MessageID> = emptyList(),
         body: List<String> = emptyList()
-    ) {
+    ) : String {
         logger.warn(message)
-        onEvent(createEvent(message, messagesIds, body = body), this)
+        val event = createEvent(message, messagesIds, body = body)
+        onEvent(event, this)
+        return event.id
     }
 
-    protected fun String?.onErrorEvent(message: String, messagesIds: List<MessageID> = emptyList(), cause: Throwable? = null) {
+    protected fun String?.onErrorEvent(message: String, messagesIds: List<MessageID> = emptyList(), cause: Throwable? = null): String {
         logger.error(cause) { "$message. Messages: ${messagesIds.joinToString(", ") {
             "${it.connectionId.sessionAlias}:${it.direction}:${it.sequence}[.${it.subsequenceList.joinToString(".")}]"
         }}" }
-        onEvent(createEvent(message, messagesIds, FAILED, cause), this)
+        val event = createEvent(message, messagesIds, FAILED, cause)
+        onEvent(event, this)
+        return event.id
     }
 
     protected fun Set<String>.onEvent(
         message: String,
         messagesIds: List<MessageID> = emptyList(),
         body: List<String> = emptyList()
-    ) = when (isEmpty()) {
-        true -> null.onEvent(message, messagesIds, body)
-        false -> forEach {
-            it.onEvent(message, messagesIds, body)
+    ) {
+        val warnEvent = null.onEvent(message, messagesIds, body)
+        forEach {
+            it.addReferenceTo(warnEvent, message, PASSED)
         }
     }
 
-    protected fun Set<String>.onErrorEvent(message: String, messagesIds: List<MessageID> = emptyList(), cause: Throwable? = null) = when (isEmpty()) {
-        true -> null.onErrorEvent(message, messagesIds, cause)
-        false -> forEach {
-            it.onErrorEvent(message, messagesIds, cause)
+    protected fun Set<String>.onErrorEvent(
+        message: String,
+        messagesIds: List<MessageID> = emptyList(),
+        cause: Throwable? = null
+    ) {
+        val errorEventId = null.onErrorEvent(message, messagesIds, cause)
+        forEach {
+            it.addReferenceTo(errorEventId, message, FAILED)
         }
     }
 
@@ -79,6 +88,19 @@ abstract class AbstractCodecProcessor(
                 else -> onEvent("[WARNING] Message $action produced ${warnings.size} warnings", messagesIds(), body = warnings)
             }
         }
+    }
+
+    private fun String.addReferenceTo(eventId: String, name: String, status: Status) {
+        onEvent(
+            Event
+                .start()
+                .endTimestamp()
+                .name(name)
+                .type(if (status != PASSED) "Error" else "Warn")
+                .bodyData(EventUtils.createMessageBean("This event contains reference to the codec event"))
+                .bodyData(ReferenceToEvent(eventId)),
+            this
+        )
     }
 
     private fun createEvent(
@@ -100,6 +122,15 @@ abstract class AbstractCodecProcessor(
         if (body.isNotEmpty()) {
             bodyData(EventUtils.createMessageBean("Information:"))
             body.forEach { bodyData(EventUtils.createMessageBean(it)) }
+        }
+    }
+
+    private class ReferenceToEvent(val eventId: String) : IBodyData {
+        val type: String
+            get() = TYPE
+
+        companion object {
+            const val TYPE = "reference"
         }
     }
 }
