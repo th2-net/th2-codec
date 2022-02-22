0282d3012a68f7127669dadac2dbbe8e4ed1df01
Nikita Shaposhnikov <topru123@gmail.com>
2022-02-22 19:53:42 +0300

    Copyright added to TestUtil.kt Refactoring Deprecated annotation added to methods of codec without context

--- src/main/kotlin/com/exactpro/th2/codec/api/IPipelineCodec.kt
+++ src/main/kotlin/com/exactpro/th2/codec/api/IPipelineCodec.kt
@@ -19,9 +19,11 @@ package com.exactpro.th2.codec.api
 import com.exactpro.th2.common.grpc.MessageGroup
 
 interface IPipelineCodec : AutoCloseable {
+    @Deprecated("Deprecated due new functionality", ReplaceWith("encode(MessageGroup, IReportingContext)"))
     fun encode(messageGroup: MessageGroup): MessageGroup = TODO("encode(messageGroup) method is not implemented")
     fun encode(messageGroup: MessageGroup, context: IReportingContext): MessageGroup = encode(messageGroup)
 
+    @Deprecated("Deprecated due new functionality", ReplaceWith("decode(MessageGroup, IReportingContext)"))
     fun decode(messageGroup: MessageGroup): MessageGroup = TODO("decode(messageGroup) method is not implemented")
     fun decode(messageGroup: MessageGroup, context: IReportingContext): MessageGroup = decode(messageGroup)
     override fun close() {}
