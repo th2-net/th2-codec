a47bed6cc400a4df92e41c9e85a1f26490923f71
Oleg Smelov <lumbervessel@gmail.com>
2022-06-05 19:15:11 +0400

    case-insensitive protocol names

--- src/main/kotlin/com/exactpro/th2/codec/AbstractCodecProcessor.kt
+++ src/main/kotlin/com/exactpro/th2/codec/AbstractCodecProcessor.kt
@@ -8,8 +8,10 @@ abstract class AbstractCodecProcessor(
     protected val eventProcessor: AbstractEventProcessor
 ) : MessageProcessor<MessageGroupBatch, MessageGroupBatch> {
     protected fun Collection<String>.checkAgainstProtocols(incomingProtocols: Collection<String>) = when {
-        incomingProtocols.none { it.isBlank() || it in this }  -> false
+        incomingProtocols.none { it.isBlank() || containsIgnoreCase(it) } -> false
         incomingProtocols.any(String::isBlank) && incomingProtocols.any(String::isNotBlank) -> error("Mixed empty and non-empty protocols are present. Asserted protocols: $incomingProtocols")
         else -> true
     }
+
+    private fun Collection<String>.containsIgnoreCase(item: String) = any { item.equals(it, true) }
 }
